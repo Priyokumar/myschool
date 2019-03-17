e15184c14dd484c6d650f4f6236b72ec32e6b71f
Priyokumar <priyon999@gmail.com>
2019-03-17 12:12:22 +0530

    Add Script

---
+++ db-script/initial/hiber.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM myschool.hibernate_sequence
+LIMIT 0, 1000
+
+-- Date: 2019-03-17 11:55
+*/
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES (21);
---
+++ db-script/initial/user_role.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM myschool_test.sc_role
+LIMIT 0, 1000
+
+-- Date: 2019-03-17 11:47
+*/
+INSERT INTO `sc_role` (`sc_id`,`name`,`created_by`,`created_date`,`updated_by`,`updated_date`,`role_desc`) VALUES (2,'Super Admin',NULL,NULL,NULL,NULL,'Super admin');
