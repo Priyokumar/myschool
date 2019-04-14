992b3770ea3b4d0b66b3e26f0517faaac8a60f4d
Priyokumar <priyon999@gmail.com>
2019-04-14 16:35:01 +0530

    Adding site

--- myschool-node/index.js
+++ myschool-node/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'dist/myschool-client')));
+app.use(express.static(path.join(__dirname, 'dist/myschool-site')));
+app.use("/admin", express.static(path.join(__dirname, 'dist/myschool-client')));
 
 const port = process.env.PORT || 80;
 app.listen(port, '0.0.0.0', () => console.log(`\nApp is running on ${port}!`));
