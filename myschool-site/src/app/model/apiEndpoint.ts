4540b0bff16962385a2db257f65780a84fe63fbb
Priyokumar <priyon999@gmail.com>
2019-04-22 23:19:34 +0530

    0.5

--- myschool-site/src/app/model/apiEndpoint.ts
+++ myschool-site/src/app/model/apiEndpoint.ts
@@ -1,8 +1,8 @@
 
 export class ApiEndpoint {
 
-    //public static BASE_URL = "http://www.pesl.org.in:8080";
-    public static BASE_URL = "http://localhost:8080";
+    public static BASE_URL = "http://www.pesl.org.in:8080";
+    //public static BASE_URL = "http://localhost:8080";
 
     public static SITE = ApiEndpoint.BASE_URL + "/site";
     public static CONTACT_US = ApiEndpoint.SITE + "/contact-us";
